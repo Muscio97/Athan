3ea117f3a6eb9b6c257354fce22a32eb816bf53a
Wiebe van Breukelen <wiebe@vanbreukelen.me>
2017-08-10 12:38:36 +0200

    Changed build env

--- cmake-build-debug/CMakeFiles/gas.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gas.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gas.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
